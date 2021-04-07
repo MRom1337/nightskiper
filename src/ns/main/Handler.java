7ab41ec2c77deb1ef82740a87e733f7b93a85a09
MRom1337 <39014465+MRom1337@users.noreply.github.com>
2021-04-07 11:38:51 +0300

    Revert "Update 0.2.0"
    This reverts commit d83d90dd19f7815b438e6071a26360b6c7687855.

--- src/ns/main/Handler.java
+++ src/ns/main/Handler.java
@@ -7,20 +7,16 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerBedLeaveEvent;
-import org.bukkit.scheduler.BukkitRunnable;
-
 
 
 
 public class Handler implements Listener {
-	private static MainClass plugin;
+	private MainClass plugin;
 	public static int NowSleep;
 	long Time;
 	long Time2;
-	public static int cooldown;
-	private final CooldownManager cooldownManager = new CooldownManager();
 	public int PlayersOnline;
-	public static float NeedToSkip;
+	public static float Half;
 	public Handler(MainClass plugin) {
 		this.plugin = plugin;
 	}
@@ -28,53 +24,18 @@ public class Handler implements Listener {
 	public void PlayerIsSleeping(PlayerBedEnterEvent e) {
 		PlayersOnline = Bukkit.getOnlinePlayers().size();
 		Time = Bukkit.getServer().getWorld("world").getTime(); 
-		cooldown = plugin.getConfig().getInt("msgcooldown");
-		Player p = e.getPlayer();
-		int timeLeft = cooldownManager.getCooldown(p.getUniqueId());
-        //e.getPlayer().sendMessage(Double.toString(distance));
-		if(Time >= 12544 && Time <= 23460) {
-			if(e.getBedEnterResult() == PlayerBedEnterEvent.BedEnterResult.OK) {
-			NowSleep = NowSleep + 1;
-			if(plugin.getConfig().getString("playermustsleep").equalsIgnoreCase("half")) {
-            	NeedToSkip = PlayersOnline/2;
-            }
-            if(plugin.getConfig().getString("playermustsleep").equalsIgnoreCase("quarter")) {
-            	NeedToSkip = PlayersOnline/4;
-            }
-            if(plugin.getConfig().getString("playermustsleep").equalsIgnoreCase("all")) {
-            	NeedToSkip = PlayersOnline;
-            }
-            if(plugin.getConfig().getString("playermustsleep").equalsIgnoreCase("custom")) {
-            	NeedToSkip = plugin.getConfig().getInt("playersneed");
-            }
-			if(timeLeft == 0){
-                cooldownManager.setCooldown(p.getUniqueId(), cooldown);
-                new BukkitRunnable() {
-                    public void run() {
-                        int timeLeft = cooldownManager.getCooldown(p.getUniqueId());
-                        cooldownManager.setCooldown(p.getUniqueId(), --timeLeft);
-                        if(timeLeft == 0){
-                            this.cancel();
-                        }
-                    }
-                }.runTaskTimer(this.plugin, 20, 20);
-                if(PlayersOnline != 1) {
-        			Bukkit.getServer().broadcastMessage("["+ NowSleep + "/" + (int)NeedToSkip + "] " + p.getName() + ChatColor.YELLOW + " now sleeping");
-        			}else {
-        			Bukkit.getServer().broadcastMessage("["+ PlayersOnline + "/" + "1" + "] " + p.getName() + ChatColor.YELLOW + " now sleeping");
-        			}
-                
-            }
 
-			
-			
+		if(Time >= 12544 && Time <= 23460) {
+			NowSleep = NowSleep + 1;
+			Player p = e.getPlayer();
+			Half = PlayersOnline/2;
+			if(PlayersOnline != 1) {
+			Bukkit.getServer().broadcastMessage("["+ NowSleep + "/" + (int)Half + "] " + p.getName() + ChatColor.YELLOW + " now sleeping");
+			}else {
+			Bukkit.getServer().broadcastMessage("["+ PlayersOnline + "/" + "1" + "] " + p.getName() + ChatColor.YELLOW + " now sleeping");
 			}
-		}	
-		
+		}
 			
-	}
-	public static void getcooldown() {
-		cooldown = plugin.getConfig().getInt("msgcooldown");
 	}
 	@EventHandler
 	public void onPlayerBedEnter(PlayerBedLeaveEvent e) {
