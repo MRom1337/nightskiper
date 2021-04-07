d83d90dd19f7815b438e6071a26360b6c7687855
MRom1337 <39014465+MRom1337@users.noreply.github.com>
2021-04-07 11:36:54 +0300

    Update 0.2.0
    - Fixed some bugs
    - Added config file
    - Fixed some ways to spam

--- src/ns/main/MainClass.java
+++ src/ns/main/MainClass.java
@@ -1,26 +1,51 @@
 package ns.main;
 
+import java.io.File;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+
+
 
 public class MainClass extends JavaPlugin{
     public boolean TS;
 	int Task;
     long Time;
     long Time2;
+    int CTime;
+    public static int NeedToSkip;
+    public static int cooldown;
+    public static MainClass instance;
 	public void onEnable() {
+		File config = new File(getDataFolder() + File.separator + "config.yml");
+		if(!config.exists()) {
+			getConfig().options().copyDefaults(true);
+			saveDefaultConfig();
+		}
 		TS = false;
+		instance = this;
 		getLogger().info("Plugin ready to work");
 		Bukkit.getPluginManager().registerEvents(new Handler(this), this);
 		Task = Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
             public void run() {
             Time = Bukkit.getServer().getWorld("world").getTime(); 
             int PlayersOnline = Bukkit.getOnlinePlayers().size();
-            int Half = PlayersOnline/2;
-            if(Handler.NowSleep >= Half && PlayersOnline > 1) {
+            if(getConfig().getString("playermustsleep").equalsIgnoreCase("half")) {
+            	NeedToSkip = PlayersOnline/2;
+            }
+            if(getConfig().getString("playermustsleep").equalsIgnoreCase("quarter")) {
+            	NeedToSkip = PlayersOnline/4;
+            }
+            if(getConfig().getString("playermustsleep").equalsIgnoreCase("all")) {
+            	NeedToSkip = PlayersOnline;
+            }
+            if(getConfig().getString("playermustsleep").equalsIgnoreCase("custom")) {
+            	NeedToSkip = getConfig().getInt("playersneed");
+            }
+            if(Handler.NowSleep >= NeedToSkip && PlayersOnline > 1) {
 				if(TS == false) {
-             	Time2 = Time + 80;
+             	Time2 = Time + CTime;
              	TS = true;
 				}
             	if(Time > Time2) {
@@ -31,5 +56,9 @@ public class MainClass extends JavaPlugin{
             }
             }
         }, 0, 10);
+	}
+	
+	public static JavaPlugin getInstance() {
+		return instance;
 	}
 }
